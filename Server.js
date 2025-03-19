36ba0afaf2a6a4843c52e61ba9e9d1edfc2d2203
VIJAYLAKSHMEE <vijuharishetter@gmail.com>
2025-03-19 14:46:31 +0530

    second change

--- Server.js
+++ Server.js
@@ -15,7 +15,7 @@ connectDB()
 app.get('/',(req,res)=>{
     res.send(`Hello,World!`)
 })
-const port = 5000
+const port =5000
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`)
 })
